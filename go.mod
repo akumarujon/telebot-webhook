ed1a5d7495892014baba833f7035b679ea485440
akumaru <isakulovdev@gmail.com>
2024-02-17 01:50:37 +0500

    simple working example

---
+++ go.mod
@@ -0,0 +1,8 @@
+module tg-bot-go
+
+go 1.22.0
+
+require (
+	github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.24 // indirect
+	gopkg.in/telebot.v3 v3.2.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	host := "url"
+	pref := tele.Settings{
+		Token: "Token Here",
+		Poller: &tele.LongPoller{
+			Timeout: 10 * time.Second,
+		},
+	}
+
+	b, err := tele.NewBot(pref)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Handle("/start", func(c tele.Context) error {
+		return c.Send("Hello")
+	})
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc(fmt.Sprintf("POST /%s", pref.Token), func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
+
+		if err != nil {
+			log.Println("Failed to parse update:", err)
+			return
+		}
+
+		var update tele.Update
+		if err := json.Unmarshal(body, &update); err != nil {
+			log.Println("Failed to parse update:", err)
+			return
+		}
+
+		b.ProcessUpdate(update)
+	})
+
+	mux.HandleFunc("GET /webhook", func(w http.ResponseWriter, r *http.Request) {
+		webhook := tele.Webhook{
+			Listen: host,
+			Endpoint: &tele.WebhookEndpoint{
+				PublicURL: fmt.Sprintf("%s/%s", host, pref.Token),
+			},
+		}
+		b.SetWebhook(&webhook)
+
+		w.Write([]byte("OK, Set"))
+	})
+
+	http.ListenAndServe("localhost:8000", mux)
+}
